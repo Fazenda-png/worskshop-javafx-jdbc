12f7ed4393efb92040beefaa06a476320dd538a2
Fazenda-jpg <fazenda.j.p.s@hotmail.com>
2021-07-26 21:42:18 -0300

    Adding database access

--- javafx-jdbc/src/model/services/DeparmentServices.java
+++ javafx-jdbc/src/model/services/DeparmentServices.java
@@ -1,18 +1,16 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DeparmentServices {
 
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+
 	public List<Department> findAll() {
-		List<Department> list = new ArrayList();
-		list.add(new Department(1, "TESTE"));
-		list.add(new Department(2, "TESTE1"));
-		list.add(new Department(3, "TESTE2"));
-		list.add(new Department(4, "TESTE3"));
-		return list;
+		return dao.findAll();
 	}
 }
